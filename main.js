df308735eccb8745813290be702fa18ef166d7ae
hectorrosa0 <127422444+hectorrosa0@users.noreply.github.com>
2024-06-20 09:44:01 -0300

    20/06/2024

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
+const contador = document.querySelectorAll(".contador")
+contador[0].textContent = "ola"
+const tempoObjetivo0 =new Date(2024,05,20,09,50)
+const tempoObjetivo1 =new Date(2024,05,20,09,50)
+const tempoObjetivo2 =new Date(2024,05,20,09,50)
+const tempoObjetivo3 =new Date(2024,05,20,09,50)
+
+let agora = new Date();
+contador[0].textContent= tempoObjetivo0-agora
+contador[1].textContent= tempoObjetivo1-agora
+contador[2].textContent= tempoObjetivo2-agora
+contador[3].textContent= tempoObjetivo3-agora
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
