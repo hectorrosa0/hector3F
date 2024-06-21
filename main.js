142b546b013c096ed02abf589a2c0f6cb90d0de9
hectorrosa0 <127422444+hectorrosa0@users.noreply.github.com>
2024-06-21 08:58:11 -0300

    21/06/2024

--- main.js
+++ main.js
@@ -3,16 +3,28 @@ const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const contador = document.querySelectorAll(".contador")
 contador[0].textContent = "ola"
-const tempoObjetivo0 =new Date(2024,05,20,09,50)
-const tempoObjetivo1 =new Date(2024,05,20,09,50)
-const tempoObjetivo2 =new Date(2024,05,20,09,50)
-const tempoObjetivo3 =new Date(2024,05,20,09,50)
+const tempoObjetivo0 =new Date(2024,05,22,09,50)
+const tempoObjetivo1 =new Date(2024,05,22,09,50)
+const tempoObjetivo2 =new Date(2024,05,22,09,50)
+const tempoObjetivo3 =new Date(2024,05,22,09,50)
+
+let segundos
+let minutos
+let horas
+let dias
 
 let agora = new Date();
+segundos =(tempoObjetivo0-agora)/1000;
+minutos = segundos/60
+horas = minutos/60
+dias = horas/24
+
 contador[0].textContent= tempoObjetivo0-agora
 contador[1].textContent= tempoObjetivo1-agora
 contador[2].textContent= tempoObjetivo2-agora
 contador[3].textContent= tempoObjetivo3-agora
+
+contador[0].textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
